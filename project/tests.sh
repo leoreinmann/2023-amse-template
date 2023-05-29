c6d703b5d0310e8c3cad25162184b92369d96b67
leoreinmann <leo@reinmann.me>
2023-05-29 15:19:09 +0200

    Added additional tests: existence of tables and existences of data is tables #2

--- project/tests.sh
+++ project/tests.sh
@@ -20,3 +20,83 @@ else
     echo "Output file is missing!"
     exit 1
 fi
+
+# Perform additional tests
+
+# Test if the 'bikes' table exists in the database
+python3 -c "
+import sqlite3
+
+# Connect to the database
+conn = sqlite3.connect('data/data.sqlite')
+c = conn.cursor()
+
+# Check if the 'bikes' table exists
+c.execute('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'bikes\'')
+table_exists = c.fetchone()
+
+if table_exists:
+    print('\'bikes\' table exists in the database')
+else:
+    print('\'bikes\' table does not exist in the database')
+
+conn.close()
+"
+
+# Test if the 'air' table exists in the database
+python3 -c "
+import sqlite3
+
+# Connect to the database
+conn = sqlite3.connect('data/data.sqlite')
+c = conn.cursor()
+
+# Check if the 'air' table exists
+c.execute('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'air\'')
+table_exists = c.fetchone()
+
+if table_exists:
+    print('\'air\' table exists in the database')
+else:
+    print('\'air\' table does not exist in the database')
+
+conn.close()
+"
+
+# Test if the 'bikes' table has data
+python3 -c "
+import sqlite3
+import pandas as pd
+
+# Connect to the database
+conn = sqlite3.connect('data/data.sqlite')
+
+# Read the 'bikes' table into a DataFrame
+df_bikes = pd.read_sql_query('SELECT * FROM bikes', conn)
+
+if not df_bikes.empty:
+    print('\'bikes\' table has data')
+else:
+    print('\'bikes\' table is empty')
+
+conn.close()
+"
+
+# Test if the 'air' table has data
+python3 -c "
+import sqlite3
+import pandas as pd
+
+# Connect to the database
+conn = sqlite3.connect('data/data.sqlite')
+
+# Read the 'air' table into a DataFrame
+df_air = pd.read_sql_query('SELECT * FROM air', conn)
+
+if not df_air.empty:
+    print('\'air\' table has data')
+else:
+    print('\'air\' table is empty')
+
+conn.close()
+"
