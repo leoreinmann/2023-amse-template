e21ece80b27c832655b3fdeb3bcd15ed2433ddca
Leo Reinmann <leo@reinmann.me>
2023-06-17 10:22:06 +0200

    read to sql is now working, datatypes must be changed

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,19 +1,19 @@
 import pandas as pd
 from sqlalchemy import create_engine
-import numpy as np
+from sqlalchemy.types import BIGINT, TEXT, FLOAT
 
 
 column_mapping = {
-    0: str,
-    1: str,
-    2: str,
-    12: np.int32,
-    22: np.int32,
-    32: np.int32,
-    42: np.int32,
-    52: np.int32,
-    62: np.int32,
-    72: np.int32
+    0: TEXT,
+    1: TEXT,
+    2: TEXT,
+    3: BIGINT,
+    4: BIGINT,
+    5: BIGINT,
+    6: BIGINT,
+    7: BIGINT,
+    8: BIGINT,
+    9: BIGINT
 }
 
 # List of column indices to keep
@@ -22,14 +22,29 @@ indices_to_keep = [0, 1, 2, 12, 22, 32, 42, 52, 62, 72]
 
 df = pd.read_csv('https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv',
                   encoding="iso-8859-1",  delimiter=';',
-                  skiprows=6, usecols=indices_to_keep, dtype=column_mapping)
+                  skiprows=6, usecols=indices_to_keep, dtype=str)
 
 
 # Remove the last 4 rows
 df = df.iloc[:-4]
 
+# rename
+mapping = {df.columns[0]: 'date',
+           df.columns[1]: 'CIN', 
+           df.columns[2]: 'name', 
+           df.columns[3]: 'petrol', 
+           df.columns[4]: 'diesel', 
+           df.columns[5]: 'gas', 
+           df.columns[6]: 'electro', 
+           df.columns[7]: 'hybrid', 
+           df.columns[8]: 'plugInHybrid', 
+           df.columns[9]: 'others'}
 
-print(df.iloc[:, 4])
+df = df.rename(columns=mapping)
+
+
+
+
 
 
 
@@ -45,15 +60,6 @@ column_mapping = {
     62: 'plugInHybrid',
     72: 'others'
 }
-
-# List of column indices to keep
-indices_to_keep = [0, 1, 2, 12, 22, 32, 42, 52, 62, 72]
-
-# Drop columns not in the list
-indices_to_drop = set(range(df.shape[1])) - set(indices_to_keep)
-
-#df = df.drop(df.columns[list(indices_to_drop)], axis=1)
-
 
 
 engine = create_engine('sqlite:///cars.sqlite')
